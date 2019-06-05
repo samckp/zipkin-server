067cff94da9e2861a3dec92613f49dca648bc6dc
samckp <sam.mca.liet@gmail.com>
2019-06-05 23:38:18 +0530

    zipkin server application with springboot

---
+++ src/main/java/com/sam/zipkin/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,12 @@
+package com.sam.zipkin.zipkinserver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+}
